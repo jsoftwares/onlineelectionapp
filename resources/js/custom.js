1cb56ee13bb6883ec7309f7f44db238cb18d39d2
Jeffrey Onochie <jeffreyonochie@gmail.com>
2020-12-11 11:36:16 +0100

    Add API authentication

---
+++ resources/js/custom.js
@@ -0,0 +1,8 @@
+export const HTTP = axios.create({
+    // baseURL: process.env.MIX_APP_URL,
+    headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer wNHGPf6QUt22z8qyGIh4l2OA3GJa0UP8DJwDADte',
+        ContentType: 'application/json',
+    }
+})
